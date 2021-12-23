65335583b582a2b8dc6d1d171f794df06a8fcdf3
nh <hnr.leto11@gmail.com>
2021-12-23 10:05:23 -0500

    change function declaration

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-
-
 // Calc Function
 
-let add = function(x, y) {
-    return(x+y)
+function add (x, y) {
+    return (x + y)
 }
 
-let subtract = function(x, y) {
-    return(x-y)
+function subtract (x, y) {
+    return (x - y)
 }
 
-let multiply = function(x, y) {
-    return(x*y)
+function multiply (x, y) {
+    return (x * y)
 }
 
-let divide = function(x, y){
-    return(x/y)
+function divide (x, y) {
+    return (x / y)
 }
 
-let operate = function(x, op, y){
-    if(op === "add") {
-        return(add(x, y));
-    }
-    else if(op === "subtract"){
-        return(subtract(x, y));
-    }
-    else if(op === "multiply"){
-        return(multiply(x, y));
-    }
-    else if(op === "divide"){
-        return(divide(x, y))
-    }
-    else {
-        return("ERROR")
+function operate (x, op, y) {
+    if (op === "add") {
+        return (add(x, y));
+    } else if (op === "subtract") {
+        return (subtract(x, y));
+    } else if (op === "multiply") {
+        return (multiply(x, y));
+    } else if (op === "divide") {
+        return (divide(x, y))
+    } else {
+        return ("ERROR")
     }
 }
 
