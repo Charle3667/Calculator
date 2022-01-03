4ffa3bec6cccf24297ae11cbf79d54a543eac1fe
Charlie Fordon <fordoncf@gmail.com>
2022-01-03 18:56:45 -0500

    mostly working with some bugs

--- script.js
+++ script.js
@@ -1,43 +1,87 @@
-const display = document.querySelector('#displayText');
-let displayArray = []
+const display = document.querySelector("#displayText");
+let digitArray = [];
+let operationArray = [];
 
-function updateDisplay () {
-display.innerHTML = displayArray.join("");
-};
+function updateDisplay() {
+    display.innerHTML = digitArray.join("");
+}
 
 // Button Functions
 
-const buttons = document.querySelectorAll("button");
-buttons.forEach((div) => {
+const digitButtons = document.querySelectorAll(".digit");
+digitButtons.forEach((div) => {
     div.addEventListener("mousedown", function (event) {
         event.target.style.opacity = 0.7;
     });
 });
-buttons.forEach((div) => {
+digitButtons.forEach((div) => {
     div.addEventListener("mouseup", function (event) {
         event.target.style.opacity = 1;
     });
 });
-buttons.forEach((div) => {
+digitButtons.forEach((div) => {
     div.addEventListener("mouseleave", function (event) {
         event.target.style.opacity = 1;
     });
 });
-buttons.forEach((div) => {
+digitButtons.forEach((div) => {
     div.addEventListener("mousedown", () => {
-        displayArray.push(div.className)
-        updateDisplay()
+        digitArray.push(div.innerHTML);
+        clearToggle();
+        return updateDisplay();
     });
 });
 
+const opButtons = document.querySelectorAll(".operator");
+opButtons.forEach((div) => {
+    div.addEventListener("mousedown", function (event) {
+        event.target.style.opacity = 0.7;
+    });
+});
+opButtons.forEach((div) => {
+    div.addEventListener("mouseup", function (event) {
+        event.target.style.opacity = 1;
+    });
+});
+opButtons.forEach((div) => {
+    div.addEventListener("mouseleave", function (event) {
+        event.target.style.opacity = 1;
+    });
+});
+opButtons.forEach((div) => {
+    div.addEventListener("mousedown", () => {
+        operationArray.push(display.innerHTML);
+        operationArray.push(div.innerHTML);
+        digitArray = [];
+        arrayCalc(operationArray);
+        clearToggle();
+        // operator button would push digit content and operator to array.
+    });
+});
 
+const clear = document.querySelector(".clear");
+clear.addEventListener("mousedown", function (event) {
+    event.target.style.opacity = 0.7;
+});
+clear.addEventListener("mouseup", function (event) {
+    event.target.style.opacity = 1;
+});
+clear.addEventListener("mouseleave", function (event) {
+    event.target.style.opacity = 1;
+});
+clear.addEventListener("mousedown", () => {
+    digitArray = [];
+    operationArray = [];
+    clearToggle();
+    return updateDisplay();
+});
 
 //Button Functions
 
 // Calc Function
 
 function add(x, y) {
-    return x + y;
+    return parseInt(x) + parseInt(y);
 }
 
 function subtract(x, y) {
@@ -53,13 +97,13 @@ function divide(x, y) {
 }
 
 function operate(x, op, y) {
-    if (op === "add") {
+    if (op === "+") {
         return add(x, y);
-    } else if (op === "subtract") {
+    } else if (op === "-") {
         return subtract(x, y);
-    } else if (op === "multiply") {
+    } else if (op === "x") {
         return multiply(x, y);
-    } else if (op === "divide") {
+    } else if (op === "/") {
         return divide(x, y);
     } else {
         return "ERROR";
@@ -67,3 +111,33 @@ function operate(x, op, y) {
 }
 
 // Calc Function
+
+// functions for calc
+
+function arrayCalc(array) {
+    if (array.length > 3) {
+        console.log("array calc fires");
+        let calc = operate(array[0], array[1], array[2]);
+        operationArray = [];
+        digitArray = [];
+        digitArray.push(calc);
+        console.log("new digit array " + digitArray);
+        console.log("operator array " + operationArray);
+        return updateDisplay();
+    } else {
+        console.log("Error on arrayCalc");
+    }
+}
+
+function clearToggle() {
+    if (digitArray.length === 0) {
+        return (clear.innerHTML = "AC");
+    } else {
+        return (clear.innerHTML = "C");
+    }
+}
+
+// functions for calc
+
+//take initial set of digits. when an operator is called, push digets and operator to array. when second set
+// of digits and the second oporatpr is called, the calc function will fun because array length is greater than 0.s
